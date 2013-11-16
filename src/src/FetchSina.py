90fb00a0c6061751b2492a7e4e967285c9d9e888
xuechong87 <xuechong87@gmail.com>
2013-11-16 14:52:23 +0800

    add submodule

---
+++ src/src/FetchSina.py
@@ -0,0 +1,7 @@
+'''
+Created on 2013-11-16
+抓取新浪围脖内容
+@author: xuechong
+'''
+
+
---
+++ src/src/FetchTencent.py
@@ -0,0 +1,11 @@
+'''
+Created on 2013-11-16
+腾讯围脖内容
+@author: xuechong
+'''
+
+from qqweibo import API
+
+
+if __name__ == "__main__":
+    pass
